0c50582aefdd998fded8252a046fbd90e8579e3a
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-07-20 21:46:16 +0200

    added swagger and swagger-ui dependency

--- src/main/java/com/skorupa/simplerestapi/SimplerestapiApplication.java
+++ src/main/java/com/skorupa/simplerestapi/SimplerestapiApplication.java
@@ -5,9 +5,11 @@ import com.skorupa.simplerestapi.repository.CarRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
+@EnableSwagger2
 @SpringBootApplication
 public class SimplerestapiApplication {
 
